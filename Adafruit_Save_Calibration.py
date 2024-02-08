a0c192ac33aceb729c671b611e2f7f99efa63d38
SThiele <scott.thiele89@gmail.com>
2024-02-08 13:08:27 -0500

    Rectified missing variable assignment in enter_parameters() function.

--- Adafruit_Save_Calibration.py
+++ Adafruit_Save_Calibration.py
@@ -12,7 +12,7 @@ sensor = adafruit_bno055.BNO055_I2C(i2c)
 last_val = 0xFFFF
 
 
-acc = np.zeros(3)
+acc = [0, 0, 0]
 gyro = mag = acc
 acc_not_set = True
 gyro_not_set = True
@@ -22,38 +22,42 @@ mag_not_set = True
 def enter_parameters(set, status, label):
     while status:
         for i in range(3):
-            set[i] = input("Enter {name} offset {iteration} \n".format(name=label, iteration=i + 1))
+            set[i] = int(input("Enter {name} offset {iteration} \n".format(name=label, iteration=i + 1)))
         print(set)
         string = input("Is this correct? y/n \n")
         if string == 'y':
             status = False
+            set = tuple(set)
+            return set
         else:
             pass
+
+
 
 while True:
     print("Current Settings: \n", sensor.offsets_accelerometer, sensor.offsets_gyroscope, sensor.offsets_magnetometer)
 
-    enter_parameters(acc, acc_not_set, "acceleration")
-    enter_parameters(gyro, mag_not_set, "gyroscope")
-    enter_parameters(acc, acc_not_set, "magnetometer")
+    acc = enter_parameters(acc, acc_not_set, "acceleration")
+    gyro = enter_parameters(gyro, gyro_not_set, "gyroscope")
+    mag = enter_parameters(mag, mag_not_set, "magnetometer")
 
     print("Saving accelerometer values...")
-    sensor.offsets_accelerometer = tuple(acc)
-    if sensor.offsets_accelerometer == tuple(acc):
+    sensor.offsets_accelerometer = acc
+    if sensor.offsets_accelerometer == acc:
         print("Accelerometer offset updated")
     else:
         print("Failed")
 
     print("Saving gyroscope values...")
-    sensor.offsets_gyroscope = tuple(gyro)
-    if sensor.offsets_gyroscope == tuple(gyro):
+    sensor.offsets_gyroscope = gyro
+    if sensor.offsets_gyroscope == gyro:
         print("Gyroscope offset updated")
     else:
         print("Failed")
 
     print("Saving magnetometer values...")
-    sensor.offsets_magnetometer = tuple(mag)
-    if sensor.offsets_magnetometer == tuple(mag):
+    sensor.offsets_magnetometer = mag
+    if sensor.offsets_magnetometer == mag:
         print("Magnetometer offset updated")
     else:
         print("Failed")
